4f1934e01835d264f5510a2905b7170cbce6a3a0
Dave Polansky <dtpolansky@gmail.com>
2017-02-08 18:50:05 -0800

    Added setup script to provision vagrant vm

---
+++ setup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Script for provisioning a vagrant vm
+
+# install go and setup bashrc
+mkdir -p /home/vagrant/bin
+curl -sL -o /home/vagrant/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+chmod +x /home/vagrant/bin/gimme
+
+gimme_command='$(GIMME_GO_VERSION=1.7 /home/vagrant/bin/gimme)'
+
+cat >> /home/vagrant/.bashrc << EOF
+eval $gimme_command
+export GOPATH=/home/vagrant
+EOF
+
+source /home/vagrant/.bashrc
+
+# install docker https://docs.docker.com/engine/installation/linux/ubuntu/
+sudo apt-get update
+sudo apt-get install curl \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual \
+    apt-transport-https \
+    software-properties-common \
+    ca-certificates
+
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+
+sudo apt-get update
+sudo apt-get -y install docker-engine
+
+sudo groupadd docker
+sudo gpasswd -a vagrant docker
+sudo service docker restart
+sudo newgrp docker
+
+# build images
+IMAGE_DIR_PATH="/home/vagrant/src/github.com/dpolansky/ci/worker/build"
+docker build -t build-golang $IMAGE_DIR_PATH/golang
+docker build -t build-test $IMAGE_DIR_PATH/test
+
+# install rabbitmq https://www.rabbitmq.com/install-debian.html
+echo 'deb http://www.rabbitmq.com/debian/ testing main' |
+        sudo tee /etc/apt/sources.list.d/rabbitmq.list
+
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc |
+        sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get -y install rabbitmq-server
+
