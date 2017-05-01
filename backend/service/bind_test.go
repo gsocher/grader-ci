a98ff89972d3891fc587b009930bca67d7cec60e
Dave Polansky <dtpolansky@gmail.com>
2017-05-01 10:36:55 -0700

    initial bind service test

---
+++ backend/service/bind_test.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"database/sql"
+	"reflect"
+	"testing"
+
+	"github.com/dpolansky/grader-ci/model"
+)
+
+var conn *sql.DB
+
+func init() {
+	var err error
+	conn, err = sql.Open("sqlite3", model.SQLiteFilepath)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	binder, err := NewSQLiteTestBindReadWriter(conn)
+	if err != nil {
+		t.Fatalf("Failed to create bind service: %v", err)
+	}
+
+	expected := &model.TestBind{
+		SourceID:   0,
+		TestID:     0,
+		TestBranch: "master",
+	}
+
+	err = binder.UpdateTestBind(expected)
+	if err != nil {
+		t.Fatalf("Failed to upsert test bind: %v", err)
+	}
+
+	actual, err := binder.GetTestBindBySourceRepositoryID(expected.SourceID)
+	if err != nil {
+		t.Fatalf("Failed to get test bind by source id: %v", err)
+	}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("Get bind by source ID failed, expected %+v got %+v", expected, actual)
+	}
+}
