0aa1ef6e5e76702240962fc529dc6447860c2f11
Dave Polansky <dtpolansky@gmail.com>
2017-05-01 10:36:39 -0700

    moved create db code out of cmd to use in tests

---
+++ backend/db/create.go
@@ -0,0 +1,72 @@
+package db
+
+import "database/sql"
+
+func CreateSQLiteTables(db *sql.DB) error {
+	tables := []func(*sql.DB) error{
+		createReposTable,
+		createBuildsTable,
+		createTestBindsTable,
+	}
+
+	for _, f := range tables {
+		err := f(db)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func createReposTable(db *sql.DB) error {
+	table := `
+	CREATE TABLE IF NOT EXISTS repos(
+		id INTEGER NOT NULL PRIMARY KEY,
+		name TEXT NOT NULL,
+		owner TEXT NOT NULL,
+		avatar_url TEXT NOT NULL
+	);
+	`
+
+	_, err := db.Exec(table)
+	return err
+}
+
+func createTestBindsTable(db *sql.DB) error {
+	table := `
+	CREATE TABLE IF NOT EXISTS test_binds(
+		source_id INTEGER NOT NULL PRIMARY KEY,
+		test_id  INTEGER NOT NULL,
+		test_branch TEXT NOT NULL,
+		FOREIGN KEY (source_id) REFERENCES repos(id),
+		FOREIGN KEY (test_id) REFERENCES repos(id)
+	);
+	`
+
+	_, err := db.Exec(table)
+	return err
+}
+
+func createBuildsTable(db *sql.DB) error {
+	table := `
+	CREATE TABLE IF NOT EXISTS builds(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		repo_id INTEGER NOT NULL,
+		repo_url TEXT NOT NULL,
+		repo_branch TEXT NOT NULL,
+		tested boolean,
+		test_id INTEGER,
+		test_url TEXTL,
+		test_branch TEXT,
+		status text NOT NULL,
+		date DATETIME NOT NULL,
+		log text,
+		FOREIGN KEY (repo_id) REFERENCES repos(id),
+		FOREIGN KEY (test_id) REFERENCES repos(id)
+	);
+	`
+
+	_, err := db.Exec(table)
+	return err
+}
--- cmd/create-sqlite-tables/main.go
+++ cmd/create-sqlite-tables/main.go
@@ -4,74 +4,22 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/dpolansky/grader-ci/backend/db"
 	"github.com/dpolansky/grader-ci/model"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	log.Printf("Opening SQLite connection\n")
-	db, err := sql.Open("sqlite3", model.SQLiteFilepath)
-	must(err)
+	conn, err := sql.Open("sqlite3", model.SQLiteFilepath)
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
 
 	log.Printf("Creating tables\n")
-	must(createReposTable(db))
-	must(createBuildsTable(db))
-	must(createTestBindsTable(db))
-}
-
-func createReposTable(db *sql.DB) error {
-	table := `
-	CREATE TABLE IF NOT EXISTS repos(
-		id INTEGER NOT NULL PRIMARY KEY,
-		name TEXT NOT NULL,
-		owner TEXT NOT NULL,
-		avatar_url TEXT NOT NULL
-	);
-	`
-
-	_, err := db.Exec(table)
-	return err
-}
-
-func createTestBindsTable(db *sql.DB) error {
-	table := `
-	CREATE TABLE IF NOT EXISTS test_binds(
-		source_id INTEGER NOT NULL PRIMARY KEY,
-		test_id  INTEGER NOT NULL,
-		test_branch TEXT NOT NULL,
-		FOREIGN KEY (source_id) REFERENCES repos(id),
-		FOREIGN KEY (test_id) REFERENCES repos(id)
-	);
-	`
-
-	_, err := db.Exec(table)
-	return err
-}
-
-func createBuildsTable(db *sql.DB) error {
-	table := `
-	CREATE TABLE IF NOT EXISTS builds(
-		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		repo_id INTEGER NOT NULL,
-		repo_url TEXT NOT NULL,
-		repo_branch TEXT NOT NULL,
-		tested boolean,
-		test_id INTEGER,
-		test_url TEXTL,
-		test_branch TEXT,
-		status text NOT NULL,
-		date DATETIME NOT NULL,
-		log text,
-		FOREIGN KEY (repo_id) REFERENCES repos(id),
-		FOREIGN KEY (test_id) REFERENCES repos(id)
-	);
-	`
-
-	_, err := db.Exec(table)
-	return err
-}
-
-func must(err error) {
+	err = db.CreateSQLiteTables(conn)
 	if err != nil {
 		panic(err)
 	}
